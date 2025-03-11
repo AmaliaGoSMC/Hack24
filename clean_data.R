4b8492f5c65de56248e112e997f28f433bf52f4f
Amalia Gomoiu <amalia.gomoiu1@socialmobilitycommission.gov.uk>
2025-03-11 11:29:27 +0000

    mitigation id

--- clean_data.R
+++ clean_data.R
@@ -10,9 +10,6 @@ risk <- read_csv("/Users/amalia.gomoiu1/Desktop/Hack24/data/Risks.csv", show_col
     rename_with(~ str_replace_all(tolower(.), " ", "_")) %>%
     rename_with(~ str_remove_all(tolower(.), "[ .]")) # Removes spaces and dots instead of replacing
 
-
-
-
 mitigation <- read_csv("/Users/amalia.gomoiu1/Desktop/Hack24/data/Mitigations.csv", show_col_types = F) %>%
     # get rid of end row
     filter(!`Risk ID`== "END") %>%
@@ -21,14 +18,15 @@ mitigation <- read_csv("/Users/amalia.gomoiu1/Desktop/Hack24/data/Mitigations.cs
     rename_with(~ str_remove_all(tolower(.), "[ .]")) # Removes spaces and dots instead of replacing
 
 
-# cols in common
+# cols in common in both datasets
 common_cols <- intersect(names(risk), names(mitigation))
 
 # Join the data and further clean
-data_cleaned <- left_join(risk, mitigation, by = common_cols) %>%
+data_cleaned <- left_join(risk, mitigation, by = common_cols, relationship = "many-to-many") %>%
     mutate(risk_unique_id = tolower(.$risk_unique_id),
            risk_unique_id = str_replace_all(risk_unique_id, "--r", ""), # Remove "--r"
            risk_unique_id = str_replace_all(risk_unique_id, "--", ""), # Remove "--"
-           risk_unique_id = str_trim(risk_unique_id) # Remove leading/trailing spaces
+           risk_unique_id = str_trim(risk_unique_id), # Remove leading/trailing spaces
+           unique_mitigation_id = tolower(.$unique_mitigation_id),
     ) %>%
     select(-c(`risk_id`, ))
